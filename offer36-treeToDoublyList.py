8db458ee7189d012106470dafe438f893728be17
zhangjianli <zhangjianli@xuetangx.com>
2020-10-30 19:00:51 +0800

    .

---
+++ offer36-treeToDoublyList.py
@@ -0,0 +1,18 @@
+"""
+输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的循环双向链表。要求不能创建任何新的节点，只能调整树中节点指针的指向。
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+class Solution:
+    def treeLeft(self, root):
+        ...
+
+    def treeRight(self, root):
+        ...
+
+    def treeToDoublyList(self, root):
+        ...
